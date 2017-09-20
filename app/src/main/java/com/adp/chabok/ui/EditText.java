08ffb1317af1160027c4baad4f2598d4f16602b8
ZAHRA SHAKIB <shakib.zsh@gmail.com>
2017-09-20 13:45:37 +0430

    fix issues #29

--- app/src/main/java/com/adp/chabok/ui/EditText.java
+++ app/src/main/java/com/adp/chabok/ui/EditText.java
@@ -3,11 +3,14 @@ package com.adp.chabok.ui;
 import android.content.Context;
 import android.graphics.Typeface;
 import android.util.AttributeSet;
+import android.view.KeyEvent;
 
 import com.adp.chabok.common.Constants;
 
 
-public class EditText extends android.widget.EditText {
+public class EditText extends android.support.v7.widget.AppCompatEditText {
+
+    OnCustomEventListener mListener;
 
     public EditText(Context context) {
         super(context);
@@ -26,4 +29,17 @@ public class EditText extends android.widget.EditText {
         final Typeface tf = Typeface.createFromAsset(context.getAssets(), Constants.APPLICATION_NORMAL_FONT);
         setTypeface(tf);
     }
+
+    public void setCustomEventListener(OnCustomEventListener eventListener) {
+        mListener = eventListener;
+    }
+
+    @Override
+    public boolean onKeyPreIme(int keyCode, KeyEvent event) {
+        if (mListener != null && keyCode == KeyEvent.KEYCODE_BACK && event.getAction() == KeyEvent.ACTION_DOWN) {
+            mListener.onEvent();
+        }
+        return super.onKeyPreIme(keyCode, event);
+    }
+
 }
